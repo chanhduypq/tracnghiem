a221281bbd84be71f8be7998596e4e1dcfb0ab66
chanhduypq <chanhduypq@gmail.com>
2017-09-04 00:04:48 +0700

    no message

--- library/Core/View/Helper/Add.php
+++ library/Core/View/Helper/Add.php
@@ -3,8 +3,7 @@
 /**
  * @author Trần Công Tuệ <chanhduypq@gmail.com>
  */
-class Core_View_Helper_Add extends Zend_View_Helper_Abstract 
-{
+class Core_View_Helper_Add extends Zend_View_Helper_Abstract {
 
     /**
      * function common
@@ -14,21 +13,19 @@ class Core_View_Helper_Add extends Zend_View_Helper_Abstract
      * @param Zend_View_Interface $view
      * @return string $html
      */
-    public function add($view, $label,$url) 
+    public function add($view, $label, $url) 
     {
         ?>
         <table width="100%" style="-moz-border-radius: 10px 10px 10px 10px;background: #D0E3F5;">
             <tr>           
                 <td style="width: 80%;">&nbsp;</td>
-                <td onclick="window.location = '<?php echo $url; ?>';" onmouseover="this.style.cursor = 'pointer';" style="color: blue;text-align: right;width: 20%;"><?php echo $label;?></td>
+                <td onclick="window.location = '<?php echo $url; ?>';" onmouseover="this.style.cursor = 'pointer';" style="color: blue;text-align: right;width: 20%;"><?php echo $label; ?></td>
             </tr>
         </table>
         <?php
-
     }
 
-    public function setView(Zend_View_Interface $view) 
-    {
+    public function setView(Zend_View_Interface $view) {
         $this->view = $view;
     }
 
