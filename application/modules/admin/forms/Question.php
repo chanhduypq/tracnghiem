c419ae8bacbba4d01cae5bbf8508532386e4af45
chanhduypq <chanhduypq@gmail.com>
2017-09-05 00:01:51 +0700

    no message

--- application/modules/admin/forms/Question.php
+++ application/modules/admin/forms/Question.php
@@ -6,19 +6,19 @@
  * @date: 11-10-2012
  * @company : http://dnict.vn
  * */
-class Admin_Form_Question extends Core_Form 
-{
-    public function init() 
-    {
+class Admin_Form_Question extends Core_Form {
+
+    public function init() {
         parent::init();
-        $this->buildElementsAutoForFormByTableName('question');    
-        
-        $this->getElement('content')->setLabel('Nội dung câu hỏi:');
-        
-        $this->removeElement("level");        
-        $level=new Core_Form_Element_Select('level');
+        $this->buildElementsAutoForFormByTableName('question', $primaryName);
+
+        $this->getElement('content')->setLabel('Nội dung câu hỏi:')->setUnique(true, 'question', $primaryName);
+//        $this->getElement('content')->setUnique(true,'question',$primaryName);
+
+        $this->removeElement("level");
+        $level = new Core_Form_Element_Select('level');
         $level->setValue(Default_Model_Question::SO_CAP);
-        $level->addMultiOptions(array(Default_Model_Question::SO_CAP=>'Sơ cấp',Default_Model_Question::TRUNG_CAP=>'Trung cấp',Default_Model_Question::CAO_CAP=>'Cao cấp'))->setLabel('level:')->setValue(Default_Model_Question::SO_CAP)->setSeparator('')->setRequired();
+        $level->addMultiOptions(array(Default_Model_Question::SO_CAP => 'Sơ cấp', Default_Model_Question::TRUNG_CAP => 'Trung cấp', Default_Model_Question::CAO_CAP => 'Cao cấp'))->setLabel('level:')->setValue(Default_Model_Question::SO_CAP)->setSeparator('')->setRequired();
         $this->addElement($level);
     }
 
