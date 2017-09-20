b9c7b18ad65379a36fd8ac62face630ba34ec080
chanhduypq <chanhduypq@gmail.com>
2017-09-20 09:59:08 +0700

    htmlentities khi save vào database (sửa code trong thư viện của Zend)

--- library/Core/Db/Table/Abstract.php
+++ library/Core/Db/Table/Abstract.php
@@ -281,6 +281,8 @@ abstract class Core_Db_Table_Abstract extends Zend_Db_Table_Abstract
             foreach ($data as $key => $value) {
                 if ($value == "") {
                     $data["$key"] = NULL;
+                } else {
+                    $data["$key"] = htmlentities($value);
                 }
                 if (!in_array($key, array_keys($this->info(self::METADATA)))) {
                     unset($data["$key"]);
@@ -308,6 +310,8 @@ abstract class Core_Db_Table_Abstract extends Zend_Db_Table_Abstract
             foreach ($data as $key => $value) {
                 if ($value == "") {
                     $data["$key"] = NULL;
+                } else {
+                    $data["$key"] = htmlentities($value);
                 }
                 if (!in_array($key, array_keys($this->info(self::METADATA)))) {
                     unset($data["$key"]);
