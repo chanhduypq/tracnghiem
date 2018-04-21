7210c8c6216b46decc28f343b9524f460ecae887
chanhduypq <chanhduypq@gmail.com>
2018-04-21 14:02:30 +0700

    no message

--- application/modules/demo/controllers/ApiclientController.php
+++ application/modules/demo/controllers/ApiclientController.php
@@ -20,19 +20,20 @@ class Demo_ApiclientController extends Core_Controller_Action {
 //        var_dump($json);
 
         $client = new Zend_Http_Client();        
-//        $client->setUri('http://tracnghiem.local/demo/apiserver');
-        $client->setUri('https://api-sandbox.amazon.co.jp/user/profile?access_token=Atza%7CIwEBIE0lLjnop77ydUocgzjsNAnHL_AuzfgK12K5pyufQIMgIBJsCMFUPds4-H8IBqvwbcfy4TKNIfXrMT_o1aThmVmpfXnPMphJXgZZuKs1eWkgMwoXzq3uBQ7LqVMkIfSk8niam0_U1ribtsiK_2lKuAJSxxJoNlHyMNU42dGe8RPLiqcdjr8ZaE8lPLoUkgdAtaP_fjG9VFC7UmYHxBhXoQrSajVLDZJlIaI8zCrqdsei9DSaOWMY2xUW9TyDE67YfWFyV5C2gNZF4lPDeFw81FQkmXZ85iZFIAQ3kvMqnBABhVS4Il51Z8z3T0D6j_X_iylOz7G7ZyVyu8Xb61KP4dVEetNFD68kw0V_wOLDWmHfvtPfqPlm7f4Gjumb7tu8u_yvf9csn0ONtHwrVuhPMzBy1fUyqQPrhLKDHbyTtmImchMX7vCrRUvqPsElN2NRiLeoYFmcE9b5jj3UdNjZdyDUZWFv1goGXfflwfnyHxRau2iSxJN8DrEmrkx9rOGDb08J2RiE2AgBjOws47k_ACM9aWfHFOBTbkHua-04g3bwmZ_BzZJy0qMFIa7wLGzOuD71ovO8F5vmRZbvf2zT1DPMFHtj8S6JKwZx-Qssi_e15g');
+        $client->setUri('http://tracnghiem.local/demo/apiserver/get');
+//        $client->setUri('https://api-sandbox.amazon.co.jp/user/profile?access_token=Atza%7CIwEBIE0lLjnop77ydUocgzjsNAnHL_AuzfgK12K5pyufQIMgIBJsCMFUPds4-H8IBqvwbcfy4TKNIfXrMT_o1aThmVmpfXnPMphJXgZZuKs1eWkgMwoXzq3uBQ7LqVMkIfSk8niam0_U1ribtsiK_2lKuAJSxxJoNlHyMNU42dGe8RPLiqcdjr8ZaE8lPLoUkgdAtaP_fjG9VFC7UmYHxBhXoQrSajVLDZJlIaI8zCrqdsei9DSaOWMY2xUW9TyDE67YfWFyV5C2gNZF4lPDeFw81FQkmXZ85iZFIAQ3kvMqnBABhVS4Il51Z8z3T0D6j_X_iylOz7G7ZyVyu8Xb61KP4dVEetNFD68kw0V_wOLDWmHfvtPfqPlm7f4Gjumb7tu8u_yvf9csn0ONtHwrVuhPMzBy1fUyqQPrhLKDHbyTtmImchMX7vCrRUvqPsElN2NRiLeoYFmcE9b5jj3UdNjZdyDUZWFv1goGXfflwfnyHxRau2iSxJN8DrEmrkx9rOGDb08J2RiE2AgBjOws47k_ACM9aWfHFOBTbkHua-04g3bwmZ_BzZJy0qMFIa7wLGzOuD71ovO8F5vmRZbvf2zT1DPMFHtj8S6JKwZx-Qssi_e15g');
         
-//        $client->setParameterGet(array(
-//            'id' => 1000
-//        ));        
+        $client->setParameterGet(array(
+            'id' => 1
+        ));        
         $client->setMethod(Zend_Http_Client::GET);
         var_dump($client->request()->getBody());
         echo '<br>';
-        exit;
+//        exit;
         
-        $client->setParameterPost(array(
-            'id' => 1000
+        $client->setUri('http://tracnghiem.local/demo/apiserver/post');
+        $client->setParameterGet(array(
+            'id' => 10
         ));  
         $client->setMethod(Zend_Http_Client::POST);
         var_dump($client->request()->getBody());
@@ -42,8 +43,9 @@ class Demo_ApiclientController extends Core_Controller_Action {
          * đối với method delete hoặc put
          * thi setParameterPost cũng dc, setParameterGet cũng dc
          */
+        $client->setUri('http://tracnghiem.local/demo/apiserver/delete');
         $client->setParameterPost(array(
-            'id' => 1000
+            'id' => 100
         ));  
 //        hoặc 
 //        $client->setParameterGet(array(
@@ -60,6 +62,7 @@ class Demo_ApiclientController extends Core_Controller_Action {
 //        $client->setParameterGet(array(
 //            'id' => 1000
 //        ));
+        $client->setUri('http://tracnghiem.local/demo/apiserver/put');
         $client->setMethod(Zend_Http_Client::PUT);
         var_dump($client->request()->getBody());
         
